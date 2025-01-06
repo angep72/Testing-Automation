ff7e14f487b86a2ae1c9aaf1fa37f4b52e1e1e0d
Pauline Umunyana <pauline.umunyana@moyotech.solutions>
2025-01-06 08:13:05 +0200

    adding new deleting functionality testin

--- test/specs/CRUD-Test.js
+++ test/specs/CRUD-Test.js
@@ -20,14 +20,27 @@ describe('for adding new product on my list ', () => {
         await ID.setValue('134');
         const name = await $('#__component0---main--creating-name-inner');
         await name.setValue('John Doe');
-        const email= await $('#__component0---main--creating-email-inner');
+        const email = await $('#__component0---main--creating-email-inner');
         await email.setValue('g@alustudent.com');
         const city = await $('#__component0---main--creating-city-inner');
         await city.setValue('New York');
         const occupation = await $('#__component0---main--creating-occupation-inner');
         await occupation.setValue('Doctor');
         await browser.pause(5000);
-
-
     });
+
 });
+describe('for deleting a product from my list ', () => {
+    it('Should delete product after button is clicked', async () => {
+        // Step 1: Navigate to the page
+        await browser.url('http://localhost:8080/index.html');
+        // Step 2: Wait for the page to be fully loaded
+        await browser.waitUntil(async () => {
+            return await browser.execute(() => document.readyState === 'complete');
+        }, {
+            timeout: 10000,
+            timeoutMsg: 'Page load timed out'
+        });
+        await browser.pause(5000);
+    })
+})
