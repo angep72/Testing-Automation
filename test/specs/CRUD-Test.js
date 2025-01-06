fe250cecbcd85bef0ce7838da8b67c56703d2459
Pauline Umunyana <pauline.umunyana@moyotech.solutions>
2025-01-06 09:03:28 +0200

    adding new test

--- test/specs/CRUD-Test.js
+++ test/specs/CRUD-Test.js
@@ -20,27 +20,24 @@ describe('for adding new product on my list ', () => {
         await ID.setValue('134');
         const name = await $('#__component0---main--creating-name-inner');
         await name.setValue('John Doe');
-        const email = await $('#__component0---main--creating-email-inner');
+        const email= await $('#__component0---main--creating-email-inner');
         await email.setValue('g@alustudent.com');
         const city = await $('#__component0---main--creating-city-inner');
         await city.setValue('New York');
         const occupation = await $('#__component0---main--creating-occupation-inner');
         await occupation.setValue('Doctor');
         await browser.pause(5000);
+        const saveBUtton = await $('#__button4-inner');
+        await saveBUtton.click();
+        await browser.pause(2000);
     });
-
-});
-describe('for deleting a product from my list ', () => {
-    it('Should delete product after button is clicked', async () => {
-        // Step 1: Navigate to the page
-        await browser.url('http://localhost:8080/index.html');
-        // Step 2: Wait for the page to be fully loaded
-        await browser.waitUntil(async () => {
-            return await browser.execute(() => document.readyState === 'complete');
-        }, {
-            timeout: 10000,
-            timeoutMsg: 'Page load timed out'
-        });
-        await browser.pause(5000);
+    it('should delete the product', async () => {
+        // to delete the product
+        const addButton = await $('#__button3-__component0---main--suppliers-0-inner');
+        await addButton.click();
+        const confirmButton = await $('#__button8-content');
+        await confirmButton.click();
+        await browser.pause(10000);
+        
     })
-})
+});
